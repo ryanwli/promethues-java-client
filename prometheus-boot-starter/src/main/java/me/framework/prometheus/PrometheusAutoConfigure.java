431824a81d96293addfc25a430144e275df269a0
ryanwli <ryanwli86@gmail.com>
2018-03-27 16:53:00 +0800

    add spring boot metrics

--- prometheus-boot-starter/src/main/java/me/framework/prometheus/PrometheusAutoConfigure.java
+++ prometheus-boot-starter/src/main/java/me/framework/prometheus/PrometheusAutoConfigure.java
@@ -3,6 +3,7 @@ package me.framework.prometheus;
 
 import io.prometheus.client.exporter.HTTPServer;
 import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
+import io.prometheus.client.spring.boot.SpringBootMetricsCollector;
 import me.framework.prometheus.jvm.HotspotJvmExporter;
 import me.framework.prometheus.mvc.MvcRequestConfigurer;
 import me.framework.prometheus.mvc.MvcRequestInterceptor;
@@ -12,6 +13,9 @@ import me.framework.prometheus.mybatis.SqlExecuteIntercepter;
 import me.framework.prometheus.property.PrometheusProperties;
 import org.apache.ibatis.executor.Executor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.endpoint.DataSourcePublicMetrics;
+import org.springframework.boot.actuate.endpoint.PublicMetrics;
+import org.springframework.boot.actuate.endpoint.SystemPublicMetrics;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -22,6 +26,9 @@ import org.springframework.core.Ordered;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by ryan on 2018/3/21.
@@ -39,6 +46,17 @@ public class PrometheusAutoConfigure {
     @ConditionalOnProperty(value = "prometheus.metrics.enabled", havingValue = "true", matchIfMissing = true)
     public HTTPServer prometheusRunner() throws IOException {
         return new HTTPServer(prometheusProperties.getPort());
+    }
+
+    @Bean
+    @ConditionalOnProperty(value = "prometheus.metrics.enabled", havingValue = "true", matchIfMissing = true)
+    public SpringBootMetricsCollector springBootMetricsCollector(Collection<PublicMetrics> publicMetrics) {
+        List<PublicMetrics> metricss = publicMetrics.stream()
+                .filter(p -> p instanceof DataSourcePublicMetrics || p instanceof SystemPublicMetrics)
+                .collect(Collectors.toList());
+        SpringBootMetricsCollector springBootMetricsCollector = new SpringBootMetricsCollector(metricss);
+        springBootMetricsCollector.register();
+        return springBootMetricsCollector;
     }
 
     @Bean
